465610e9787f2b74434c9c5aa652be8295084a0b
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-09-27 16:39:24 -0500

    a

--- index.php
+++ index.php
@@ -1,43 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Navbar 12</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="/css/inicio.css" />
-  </head>
-  <body>
-    <nav class="navbar">
-      <div class="logo">
-        <img src="/img/incio/" />
-        <h2>Pexels</h2>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/login.css">
+    <script src="validacion.js"></script>
+    
+    <title>Login</title>
+</head>
+
+<body> 
+    <!-- inicio de sesion -->
+    <div class="container">
+        <h1>Iniciar sesión</h1>
+        <?php
+        if (isset($_GET['mensaje'])) {
+            $mensaje = $_GET['mensaje'];
+            echo '<p class="error-message">' . $mensaje . '</p>';
+        }
+        ?>
+        <form action="/controlador/validar_login.php" method="post">
+          <label for="usuario">Usuario:</label>
+          <input type="usuario" id="usuario" name="usuario" >
+      
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" name="contraseña" >
+      
+          <button type="submit">Iniciar sesión</button>
+        </form>
+      
+        <p>¿No tienes cuenta? <a href="/Login_registro/registrarse/registrarse.php">Regístrate aquí</a>.</p>
       </div>
-      <div class="links">
-        <a href="#">Upload</a>
-        <a href="#">Licence</a>
-        <div class="dropdown">
-          <a href="#"
-            >Explore
-            <img src="/img/incio/chevron.png" />
-          </a>
-          <div class="menu">
-            <a href="#"> Discover Photos </a>
-            <a href="#"> Popular Searches </a>
-            <a href="#"> Leaderboard </a>
-            <a href="#"> Challenges </a>
-            <a href="#"> Free Videos </a>
-            <a href="#"> Pexels Blog </a>
-          </div>
-        </div>
-        <a href="/login/login.php" class="join-link"> Join </a>
-      </div>
-    </nav>
-  </body>
+
+
+    
+
+</body>
+
 </html>
