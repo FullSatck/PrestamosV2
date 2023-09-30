9ef44f3517e263da11f0adbacd0d4e38d73c23cd
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-09-30 12:37:06 -0500

    cambios a prestamos
    q

--- controllers/procesar_prestamo.php
+++ controllers/procesar_prestamo.php
@@ -13,30 +13,52 @@ $frecuencia_pago = $_POST['frecuencia_pago'];
 $zona = $_POST['zona'];
 
 // Calcular la fecha de vencimiento en función de la frecuencia de pago y el plazo
-if ($frecuencia_pago == 'diario') {
-    $fecha_vencimiento = date('Y-m-d', strtotime("+$plazo days", strtotime($fecha_inicio)));
-} elseif ($frecuencia_pago == 'semanal') {
-    $fecha_vencimiento = date('Y-m-d', strtotime("+$plazo weeks", strtotime($fecha_inicio)));
-} elseif ($frecuencia_pago == 'quincenal') {
-    $fecha_vencimiento = date('Y-m-d', strtotime("+$plazo weeks", strtotime($fecha_inicio)));
-} else {
-    $fecha_vencimiento = date('Y-m-d', strtotime("+$plazo months", strtotime($fecha_inicio)));
-}
+$fecha_vencimiento = calcularFechaVencimiento($fecha_inicio, $plazo, $frecuencia_pago);
 
-// Calcular el monto a pagar
-// Fórmula: Monto a pagar = Monto + (Monto * Tasa de Interés / 100)
-$monto_a_pagar = $monto + ($monto * $tasa_interes / 100);
+// Calcular el monto total a pagar
+// Fórmula: Monto Total a Pagar = Monto + (Monto * Tasa de Interés / 100)
+$monto_total = $monto + ($monto * $tasa_interes / 100);
+
+// Calcular el monto de cada cuota
+$cuota = $monto_total / $plazo;
 
 // Insertar la solicitud de préstamo en la base de datos
-$sql = "INSERT INTO Prestamos (IDCliente, Monto, TasaInteres, Plazo, MonedaID, FechaInicio, FechaVencimiento, Estado, CobradorAsignado, Zona, MontoAPagar) 
-        VALUES ('$id_cliente', '$monto', '$tasa_interes', '$plazo', '$moneda_id', '$fecha_inicio', '$fecha_vencimiento', 'pendiente', NULL, '$zona', '$monto_a_pagar')";
+$sql = "INSERT INTO Prestamos (IDCliente, Monto, TasaInteres, Plazo, MonedaID, FechaInicio, FechaVencimiento, Estado, CobradorAsignado, Zona, FrecuenciaPago, MontoAPagar, Cuota, MontoCuota) 
+VALUES ('$id_cliente', '$monto', '$tasa_interes', '$plazo', '$moneda_id', '$fecha_inicio', '$fecha_vencimiento', 'pendiente', NULL, '$zona', '$frecuencia_pago', '$monto_total', '$cuota', '$cuota')";
+
 
 if ($conexion->query($sql) === TRUE) {
-    echo "Solicitud de préstamo realizada con éxito. Monto a pagar: $monto_a_pagar";
+    echo "Solicitud de préstamo realizada con éxito. Monto Total a Pagar: $monto_total. Cada cuota es de: $cuota";
 } else {
     echo "Error al solicitar el préstamo: " . $conexion->error;
 }
 
 // Cerrar la conexión a la base de datos
 $conexion->close();
+
+// Función para calcular la fecha de vencimiento en función del plazo y la frecuencia de pago
+function calcularFechaVencimiento($fecha_inicio, $plazo, $frecuencia_pago) {
+    $fecha = new DateTime($fecha_inicio);
+
+    switch ($frecuencia_pago) {
+        case 'diario':
+            $fecha->add(new DateInterval('P' . $plazo . 'D'));
+            break;
+        case 'semanal':
+            $fecha->add(new DateInterval('P' . ($plazo * 7) . 'D'));
+            break;
+        case 'quincenal':
+            $fecha->add(new DateInterval('P' . ($plazo * 15) . 'D'));
+            break;
+        case 'mensual':
+            $fecha->add(new DateInterval('P' . $plazo . 'M'));
+            break;
+        default:
+            // Por defecto, se asume pago mensual
+            $fecha->add(new DateInterval('P' . $plazo . 'M'));
+            break;
+    }
+
+    return $fecha->format('Y-m-d');
+}
 ?>
