de738930321366d81acb1dfd3517c7a024d58e70
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-10-05 18:51:32 -0500

    cambiosf

--- resources/views/admin/abonos/abonos.php
+++ resources/views/admin/abonos/abonos.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,7 +13,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/public/assets/css/abonos.css">
 </head>
-
 <body>
     <!-- Barra de navegación -->
     <div class="navbar">
@@ -56,8 +54,6 @@
             <span id="monto"></span>
         </div>
 
-
-
         <div class="column">
             <label for="curp" style="color: blue;">CURP: </label> <br>
             <span id="curp"></span>
@@ -85,22 +81,14 @@
             <label for="fecha" style="color: blue;">Fecha: </label>
             <div id="fecha-actual" class="date-input"></div>
         </div>
-
-
     </div>
-
 
     <!-- Agrega un botón o formulario para registrar el pago -->
     <button id="registrarPago" class="btn-guardar">Registrar Pago</button>
 
-
-
-
-
     <script>
     // Variables globales para llevar un registro del ID del cliente actual
     var clienteActualId = 1; // Empieza con el primer cliente (puedes cambiarlo si deseas)
-
 
     // Función para cargar los datos del cliente
     function cargarDatosCliente(clienteId) {
@@ -118,11 +106,10 @@
                 $("#cuota").text(data.Cuota); // Mostrar cuota
             },
             error: function() {
-                alert("No hay mas clientes que mostrar");
+                alert("No hay más clientes que mostrar");
             }
         });
     }
-
 
     // Función para cambiar al cliente anterior
     function cambiarClienteAnterior() {
@@ -142,6 +129,7 @@
     // Manejar los eventos de los botones para cambiar de cliente
     $("#fecha-izquierda").click(cambiarClienteAnterior);
     $("#fecha-derecha").click(cambiarClienteSiguiente);
+
     // Función para redireccionar al CRUD de clientes
     function redireccionarCrudClientes() {
         // Cambia 'crud_clientes.html' por la URL de tu página de CRUD de clientes
@@ -150,49 +138,63 @@
 
     // Asocia la función al evento de clic en el botón "Menu"
     $("#menu").click(redireccionarCrudClientes);
+
     // Función para registrar un pago
     function registrarPago() {
-        // Obtener el monto a pagar y otros datos necesarios
-        var monto = parseFloat($("#pagar").val());
-        var fechaPago = $("#fecha").val();
+    // Obtener el monto a pagar y otros datos necesarios
+    var monto = parseFloat($("#pagar").val());
+    var fechaPago = $("#fecha").val();
 
-        // Realizar una solicitud AJAX para registrar el pago
-        $.ajax({
-            type: "POST",
-            url: "/resources/views/admin/abonos/registrar_pago.php",
-            data: {
-                clienteId: clienteActualId, // ID del cliente actual
-                monto: monto,
-                fechaPago: fechaPago
-            },
-            success: function(response) {
-                // Actualizar la página o mostrar un mensaje de éxito
+    // Convertir los datos en formato JSON
+    var datos = {
+        clienteId: clienteActualId, // ID del cliente actual
+        monto: monto,
+        fechaPago: fechaPago
+    };
+
+    // Realizar una solicitud AJAX para registrar el pago
+    $.ajax({
+        type: "POST",
+        url: "registrar_pago.php",
+        data: JSON.stringify(datos), // Enviar los datos en formato JSON
+        dataType: "text", // No espera una respuesta JSON
+        success: function(response) {
+            if (response == "1") {
+                // Éxito: El pago se registró correctamente
                 alert("Pago registrado con éxito");
-                // Puedes agregar aquí código adicional para actualizar la interfaz si es necesario
-            },
-            error: function() {
+                // Actualiza la página o realiza otras acciones necesarias
+            } else {
+                // Error: Muestra el mensaje de error
                 alert("Error al registrar el pago");
             }
-        });
-    }
+        },
+        error: function(xhr, status, error) {
+            // Error en la solicitud AJAX
+            alert("Error en la solicitud AJAX: " + error);
+        }
+    });
+}
+
+
 
     // Asocia la función al evento de clic en el botón "Registrar Pago"
     $("#registrarPago").click(registrarPago);
 
     // Función para obtener la fecha actual en formato deseado (dd/mm/yyyy hh:mm a)
     function obtenerFechaActual() {
-        var fecha = new Date();
-        var dia = fecha.getDate().toString().padStart(2, '0');
-        var mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // Los meses son base 0
-        var anio = fecha.getFullYear();
-        var horas = fecha.getHours().toString().padStart(2, '0');
-        var minutos = fecha.getMinutes().toString().padStart(2, '0');
-        var ampm = horas >= 12 ? 'p.m.' : 'a.m.';
-        horas = horas % 12;
-        horas = horas ? horas : 12; // Las 12:00 p.m. y las 12:00 a.m. se muestran como 12:00
-        var formatoFecha = dia + '/' + mes + '/' + anio + ' ' + horas + ':' + minutos + ' ' + ampm;
-        return formatoFecha;
-    }
+    var fecha = new Date();
+    var dia = fecha.getDate().toString().padStart(2, '0');
+    var mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // Los meses son base 0
+    var anio = fecha.getFullYear();
+    var horas = fecha.getHours().toString().padStart(2, '0');
+    var minutos = fecha.getMinutes().toString().padStart(2, '0');
+    var ampm = horas >= 12 ? 'p.m.' : 'a.m.';
+    horas = horas % 12;
+    horas = horas ? horas : 12; // Las 12:00 p.m. y las 12:00 a.m. se muestran como 12:00
+    var formatoFecha = dia + '/' + mes + '/' + anio + ' ' + horas + ':' + minutos + ' ' + ampm;
+    return formatoFecha.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
 
     // Función para mostrar la fecha actual en el div en tiempo real
     function mostrarFechaActualEnTiempoReal() {
@@ -213,7 +215,5 @@
     // Llama a la función para mostrar la fecha en tiempo real cuando se carga la página
     mostrarFechaActualEnTiempoReal();
     </script>
-
 </body>
-
 </html>
