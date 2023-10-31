a8be12098fa8ca6e57bab88ca2de0a7a0a97813a
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-10-31 12:46:00 -0500

    7777

--- resources/views/cobrador/inicio/inicio.php
+++ resources/views/cobrador/inicio/inicio.php
@@ -92,79 +92,79 @@ mysqli_close($conexion);
         <nav class="navegacion">
             <ul>
                 <li>
-                    <a href="/resources/views/admin/admin_saldo/saldo_admin.php">
+                    <a href="/resources/views/supervisor/admin_saldo/saldo_admin.php">
                         <ion-icon name="push-outline"></ion-icon>
                         <span>Saldo Inicial</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/inicio/inicio.php" class="hola">
+                    <a href="/resources/views/supervisor/inicio/inicio.php" class="hola">
                         <ion-icon name="home-outline"></ion-icon>
                         <span>Inicio</span>
                     </a>
-                </li>
+                </li> 
                 <li>
-                    <a href="/resources/views/admin/usuarios/crudusuarios.php">
+                    <a href="/resources/views/supervisor/usuarios/crudusuarios.php">
                         <ion-icon name="people-outline"></ion-icon>
                         <span>Usuarios</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/usuarios/registrar.php">
+                    <a href="/resources/views/supervisor/usuarios/registrar.php">
                         <ion-icon name="person-add-outline"></ion-icon>
                         <span>Registrar Usuario</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/clientes/lista_clientes.php">
+                    <a href="/resources/views/supervisor/clientes/lista_clientes.php">
                         <ion-icon name="people-circle-outline"></ion-icon>
                         <span>Clientes</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/clientes/agregar_clientes.php">
+                    <a href="/resources/views/supervisor/clientes/agregar_clientes.php">
                         <ion-icon name="person-circle-outline"></ion-icon>
                         <span>Registrar Clientes</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/creditos/crudPrestamos.php">
+                    <a href="/resources/views/supervisor/creditos/crudPrestamos.php">
                         <ion-icon name="list-outline"></ion-icon>
                         <span>Prestamos</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/creditos/prestamos.php">
+                    <a href="/resources/views/supervisor/creditos/prestamos.php">
                         <ion-icon name="cloud-upload-outline"></ion-icon>
                         <span>Registrar Prestamos</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/cobros/cobros.php">
+                    <a href="/resources/views/supervisor/cobros/cobros.php">
                         <ion-icon name="planet-outline"></ion-icon>
                         <span>Zonas de cobro</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/gastos/gastos.php">
+                    <a href="/resources/views/supervisor/gastos/gastos.php">
                         <ion-icon name="alert-circle-outline"></ion-icon>
                         <span>Gastos</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/abonos/lista_super.php">
+                    <a href="/resources/views/supervisor/abonos/lista_super.php">
                         <ion-icon name="map-outline"></ion-icon>
                         <span>Ruta</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/abonos/abonos.php">
+                    <a href="/resources/views/supervisor/abonos/abonos.php">
                         <ion-icon name="cloud-download-outline"></ion-icon>
                         <span>Abonos</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/resources/views/admin/retiros/retiros.php">
+                    <a href="/resources/views/supervisor/retiros/retiros.php">
                         <ion-icon name="cloud-done-outline"></ion-icon>
                         <span>Retiros</span>
                     </a>
