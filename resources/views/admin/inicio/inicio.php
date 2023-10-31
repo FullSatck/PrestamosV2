819d3a1e577a787307cad6902b06f30ffb9b2f10
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-10-31 14:02:54 -0500

    .

--- resources/views/admin/inicio/inicio.php
+++ resources/views/admin/inicio/inicio.php
@@ -67,115 +67,36 @@ mysqli_close($conexion);
 
 
 
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sidebar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recaudo</title>
     <link rel="stylesheet" href="/public/assets/css/inicio.css">
 </head>
+
 <body>
-    <div class="menu">
-        <ion-icon name="menu-outline"></ion-icon>
-        <ion-icon name="close-outline"></ion-icon>
-    </div>
 
-    <div class="barra-lateral">
-        <div>
-            <div class="nombre-pagina">
-                <ion-icon id="cloud" name="cloud-outline"></ion-icon>
-                <span>Apex</span>
-            </div>
-            <button class="boton">
-                <ion-icon name="add-outline"></ion-icon>
-                <span>Create new</span>
-            </button>
+
+<div id="sidebar">
+        <div class="sidebar-header">
+            <i id="sidebarToggle" class="sidebar-icon">&#9776;</i>
+            <h2>Recaudo</h2>
         </div>
-
-        <nav class="navegacion">
-            <ul>
-                <li>
-                    <a id="inbox" href="#">
-                        <ion-icon name="mail-unread-outline"></ion-icon>
-                        <span>Inbox</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <ion-icon name="star-outline"></ion-icon>
-                        <span>Starred</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                        <span>Sent</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <ion-icon name="document-text-outline"></ion-icon>
-                        <span>Drafts</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                        <span>Important</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <ion-icon name="alert-circle-outline"></ion-icon>
-                        <span>Spam</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <ion-icon name="trash-outline"></ion-icon>
-                        <span>Trash</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-
-        <div>
-            <div class="linea"></div>
-
-            <div class="modo-oscuro">
-                <div class="info">
-                    <ion-icon name="moon-outline"></ion-icon>
-                    <span>Drak Mode</span>
-                </div>
-                <div class="switch">
-                    <div class="base">
-                        <div class="circulo">
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="usuario">
-                <img src="/Jhampier.jpg" alt="">
-                <div class="info-usuario">
-                    <div class="nombre-email">
-                        <span class="nombre">Jhampier</span>
-                        <span class="email">jhampier@gmail.com</span>
-                    </div>
-                    <ion-icon name="ellipsis-vertical-outline"></ion-icon>
-                </div>
-            </div>
-        </div>
-
+        <ul class="sidebar-menu">
+            <li><a href="#"><i class="menu-icon">&#128205;</i><span class="menu-text"> Saldo Inicial</span></a></li>
+            <li><a href="#"><i class="menu-icon">&#127968;</i><span class="menu-text"> Inicio</span></a></li>
+            <!-- Añade más elementos con íconos y texto como se muestra arriba -->
+        </ul>
     </div>
-
-
-
     <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
 
-    <main>
+    <div id="content">
+
         <h1>Inicio Administrador</h1>
         <div class="cuadros-container">
             <div class="cuadro cuadro-1">
@@ -201,10 +122,13 @@ mysqli_close($conexion);
                 </div>
             </div>
         </div>
-    </main>
+
+    </div>
 
 
-    
+
+
+
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script src="/public/assets/js/MenuLate.js"></script>
--- resources/views/cobrador/inicio/inicio.php
+++ resources/views/cobrador/inicio/inicio.php
@@ -78,118 +78,42 @@ mysqli_close($conexion);
 </head>
 
 <body>
-    <div class="menu">
-        <ion-icon name="menu-outline"></ion-icon>
-        <ion-icon name="close-circle-outline"></ion-icon>
-    </div>
-    <div class="barra-lateral">
-        <div>
-            <div class="nombre-pagina">
-                <ion-icon id="cloud" name="wallet-outline"></ion-icon>
-                <span>Recaudo</span>
-            </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recaudo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div id="sidebar">
+        <div class="sidebar-title">
+            <h2>Recaudo</h2>
+            <span id="close-btn">&times;</span>
         </div>
-        <nav class="navegacion">
-            <ul>
-                <li>
-                    <a href="/resources/views/supervisor/admin_saldo/saldo_admin.php">
-                        <ion-icon name="push-outline"></ion-icon>
-                        <span>Saldo Inicial</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/inicio/inicio.php" class="hola">
-                        <ion-icon name="home-outline"></ion-icon>
-                        <span>Inicio</span>
-                    </a>
-                </li> 
-                <li>
-                    <a href="/resources/views/supervisor/usuarios/crudusuarios.php">
-                        <ion-icon name="people-outline"></ion-icon>
-                        <span>Usuarios</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/usuarios/registrar.php">
-                        <ion-icon name="person-add-outline"></ion-icon>
-                        <span>Registrar Usuario</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/clientes/lista_clientes.php">
-                        <ion-icon name="people-circle-outline"></ion-icon>
-                        <span>Clientes</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/clientes/agregar_clientes.php">
-                        <ion-icon name="person-circle-outline"></ion-icon>
-                        <span>Registrar Clientes</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/creditos/crudPrestamos.php">
-                        <ion-icon name="list-outline"></ion-icon>
-                        <span>Prestamos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/creditos/prestamos.php">
-                        <ion-icon name="cloud-upload-outline"></ion-icon>
-                        <span>Registrar Prestamos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/cobros/cobros.php">
-                        <ion-icon name="planet-outline"></ion-icon>
-                        <span>Zonas de cobro</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/gastos/gastos.php">
-                        <ion-icon name="alert-circle-outline"></ion-icon>
-                        <span>Gastos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/abonos/lista_super.php">
-                        <ion-icon name="map-outline"></ion-icon>
-                        <span>Ruta</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/abonos/abonos.php">
-                        <ion-icon name="cloud-download-outline"></ion-icon>
-                        <span>Abonos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/supervisor/retiros/retiros.php">
-                        <ion-icon name="cloud-done-outline"></ion-icon>
-                        <span>Retiros</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-
-        <div>
-            <div class="linea"></div>
-
-            <div class="modo-oscuro">
-                <div class="info">
-                    <ion-icon name="arrow-back-outline"></ion-icon>
-                    <a href="/controllers/cerrar_sesion.php"><span class="hola">Cerrar Sesion</span></a>
-                </div>
-            </div>
-        </div>
-
+        <ul class="sidebar-links">
+            <li><a href="#">Saldo Inicial</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Usuarios</a></li>
+            <li><a href="#">Registrar Usuario</a></li>
+            <li><a href="#">Clientes</a></li>
+            <li><a href="#">Registrar Clientes</a></li>
+            <li><a href="#">Préstamos</a></li>
+            <li><a href="#">Registrar Préstamos</a></li>
+            <li><a href="#">Zonas de cobro</a></li>
+            <li><a href="#">Gastos</a></li>
+            <li><a href="#">Ruta</a></li>
+            <li><a href="#">Abonos</a></li>
+            <li><a href="#">Retiros</a></li>
+            <li><a href="#">Cerrar sesión</a></li>
+        </ul>
     </div>
-
-
-    <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
-
-    <main>
-        <h1>Inicio Administrador</h1>
+    
+ <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
+    <div id="content">
+    <h1>Inicio Administrador</h1>
         <div class="cuadros-container">
             <div class="cuadro cuadro-1">
                 <div class="cuadro-1-1">
@@ -214,7 +138,14 @@ mysqli_close($conexion);
                 </div>
             </div>
         </div>
-    </main>
+        <span id="menu-btn">&#9776;</span>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+</html>
+
+   
 
 
 
