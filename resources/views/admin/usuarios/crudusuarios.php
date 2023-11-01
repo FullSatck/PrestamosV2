9beb0c1635fd122156df2311c1ddcad5ac145316
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-11-01 17:21:40 -0500

    barra

--- resources/views/admin/usuarios/crudusuarios.php
+++ resources/views/admin/usuarios/crudusuarios.php
@@ -217,11 +217,12 @@ if (isset($_GET['mensaje'])) {
         });
     });
     </script>
+    <script src="/public/assets/js/MenuLate.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
    
-    <script src="/public/assets/js/MenuLate.js"></script>
+    
 
 </body>
 
