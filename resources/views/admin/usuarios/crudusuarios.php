006ae8ce9f7a5126398e2d9b618ef220efe45efc
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-11-03 16:39:18 -0500

    aaa

--- resources/views/admin/usuarios/crudusuarios.php
+++ resources/views/admin/usuarios/crudusuarios.php
@@ -3,23 +3,40 @@ session_start();
 include("../../../../controllers/conexion.php");
 
 // Verifica si el usuario está autenticado
-if (isset($_SESSION["usuario_id"])) {
-    // El usuario está autenticado, puede acceder a esta página
-} else {
+if (!isset($_SESSION["usuario_id"])) {
     // El usuario no está autenticado, redirige a la página de inicio de sesión
     header("Location: ../../../../index.php");
     exit();
 }
-
 
 // Verificar si se ha pasado un mensaje en la URL
 $mensaje = "";
 if (isset($_GET['mensaje'])) {
     $mensaje = $_GET['mensaje'];
 }
+
+// Procesar el formulario de cambio de estado
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["usuario_id"]) && isset($_POST["nuevo_estado"])) {
+    $usuarioID = $_POST["usuario_id"];
+    $nuevoEstado = $_POST["nuevo_estado"];
+
+    // Actualiza el estado en la base de datos
+    $updateSQL = $conexion->prepare("UPDATE usuarios SET Estado = ? WHERE ID = ?");
+    $updateSQL->bind_param("si", $nuevoEstado, $usuarioID);
+
+    if ($updateSQL->execute()) {
+        // Éxito en la actualización, redirige de nuevo a esta página
+        header("Location: crudusuarios.php?mensaje=Estado actualizado correctamente.");
+        exit();
+    } else {
+        // Error en la actualización
+        $mensaje = "Error al actualizar el estado en la base de datos.";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -29,6 +46,7 @@ if (isset($_GET['mensaje'])) {
     <link rel="stylesheet" href="/public/assets/css/lista_usuarios.css">
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
 </head>
+
 <body id="body">
 
     <header>
@@ -39,8 +57,8 @@ if (isset($_GET['mensaje'])) {
 
     <div class="menu__side" id="menu_side">
 
-    <div class="name__page">
-        <img src="/public/assets/img/logo.png" class="img logo-image" alt="">
+        <div class="name__page">
+            <img src="/public/assets/img/logo.png" class="img logo-image" alt="">
             <h4>Recaudo</h4>
         </div>
 
@@ -122,13 +140,13 @@ if (isset($_GET['mensaje'])) {
 
             <a href="/resources/views/admin/abonos/abonos.php">
                 <div class="option">
-                <i class="fa-solid fa-money-bill-trend-up" title=""></i>
+                    <i class="fa-solid fa-money-bill-trend-up" title=""></i>
                     <h4>Abonos</h4>
                 </div>
             </a>
             <a href="/resources/views/admin/retiros/retiros.php">
                 <div class="option">
-                <i class="fa-solid fa-scale-balanced" title=""></i>
+                    <i class="fa-solid fa-scale-balanced" title=""></i>
                     <h4>Retiros</h4>
                 </div>
             </a>
@@ -139,16 +157,13 @@ if (isset($_GET['mensaje'])) {
 
     </div>
 
-
     <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
 
     <main>
         <h1>Listado de Usuarios</h1>
-
         <div class="search-container">
             <input type="text" id="search-input" class="search-input" placeholder="Buscar...">
         </div>
-
         <table>
             <tr>
                 <th>ID</th>
@@ -157,42 +172,51 @@ if (isset($_GET['mensaje'])) {
                 <th>Email</th>
                 <th>Zona</th>
                 <th>Rol</th>
+                <th>Estado</th>
                 <th>Acciones</th>
             </tr>
             <?php
             $sql = $conexion->query("SELECT * FROM usuarios");
 
-               // Verificar si la consulta se realizó con éxito
-               if ($sql === false) {
-                   die("Error en la consulta SQL: " . $conexion->error);
-               }
+            if ($sql === false) {
+                die("Error en la consulta SQL: " . $conexion->error);
+            }
 
-               // Verificar si la consulta devolvió resultados
-               if ($sql->num_rows > 0) {
-                   $rowCount = 0; // Contador de filas
-                while ($datos = $sql->fetch_object()) { 
-                       $rowCount++; // Incrementar el contador de filas
-                       ?>
-
-            <tr class="row<?= $rowCount ?>">
-                <td><?= "REC 100" .$datos->ID ?></td>
-                <td><?= $datos->Nombre ?></td>
-                <td><?= $datos->Apellido ?></td>
-                <td><?= $datos->Email ?></td>
-                <td><?= $datos->Zona ?></td>
-                <td><?= $datos->RolID ?></td>
-                <td>
-                    <a href="modificarUser.php?id=<?= $datos->ID ?>">
-                        <i class="fas fa-pencil-alt"></i> Modificar
-                    </a>
-                </td </tr>
-                <?php } 
-                        } else {
-                            echo "No se encontraron resultados.";
-                        }
-                        ?>
+            if ($sql->num_rows > 0) {
+                $rowCount = 0;
+                while ($datos = $sql->fetch_object()) {
+                    $rowCount++;
+                    ?>
+                    <tr class="row<?= $rowCount ?>">
+                        <td><?= "REC 100" . $datos->ID ?></td>
+                        <td><?= $datos->Nombre ?></td>
+                        <td><?= $datos->Apellido ?></td>
+                        <td><?= $datos->Email ?></td>
+                        <td><?= $datos->Zona ?></td>
+                        <td><?= $datos->RolID ?></td>
+                        <td>
+                            <form class="estado-form" action="" method="POST">
+                                <input type="hidden" name="usuario_id" value="<?= $datos->ID ?>">
+                                <select name="nuevo_estado">
+                                    <option value="Activo" <?php if ($datos->Estado == 'Activo') echo 'selected'; ?>>Activo</option>
+                                    <option value="Inactivo" <?php if ($datos->Estado == 'Inactivo') echo 'selected'; ?>>Inactivo</option>
+                                </select>
+                                <button type="submit">Cambiar</button>
+                            </form>
+                        </td>
+                        <td>
+                            <a href="modificarUser.php?id=<?= $datos->ID ?>">
+                                <i class="fas fa-pencil-alt"></i> Modificar
+                            </a>
+                        </td>
+                    </tr>
+                <?php
+                }
+            } else {
+                echo "No se encontraron resultados.";
+            }
+            ?>
         </table>
-
     </main>
 
     <script>
@@ -221,9 +245,9 @@ if (isset($_GET['mensaje'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
-   
-    
+
+
 
 </body>
 
-</html> 
+</html>
