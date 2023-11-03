904f20e15428178fbc9de49ecbca51d83316510d
SaturnoXVI <jcbohorquez712@gmail.com>
2023-11-03 17:07:02 -0500

    .

--- resources/views/admin/ruta/lista_super.php
+++ resources/views/admin/ruta/lista_super.php
@@ -24,142 +24,143 @@ if (isset($_GET['mensaje'])) {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/assets/css/lista_super.css">
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
     <title>Abonos</title>
 </head>
 
 <body>
-<body id="body">
 
-<header>
-    <div class="icon__menu">
-        <i class="fas fa-bars" id="btn_open"></i>
-    </div>
-</header>
+    <body id="body">
 
-<div class="menu__side" id="menu_side">
-
-    <div class="name__page">
-        <img src="/public/assets/img/logo.png" class="img logo-image" alt="">
-        <h4>Recaudo</h4>
-    </div>
-
-    <div class="options__menu">
-
-        <a href="/resources/views/admin/inicio/inicio.php" class="selected">
-            <div class="option">
-                <i class="fa-solid fa-landmark" title="Inicio"></i>
-                <h4>Inicio</h4>
+        <header>
+            <div class="icon__menu">
+                <i class="fas fa-bars" id="btn_open"></i>
             </div>
-        </a>
+        </header>
 
-        <a href=" /resources/views/admin/admin_saldo/saldo_admin.php">
-            <div class="option">
-                <i class="fa-solid fa-sack-dollar" title=""></i>
-                <h4>Saldo Incial</h4>
+        <div class="menu__side" id="menu_side">
+
+            <div class="name__page">
+                <img src="/public/assets/img/logo.png" class="img logo-image" alt="">
+                <h4>Recaudo</h4>
             </div>
-        </a>
 
-        <a href="/resources/views/admin/usuarios/crudusuarios.php">
-            <div class="option">
-                <i class="fa-solid fa-users" title=""></i>
-                <h4>Usuarios</h4>
+            <div class="options__menu">
+
+                <a href="/resources/views/admin/inicio/inicio.php" class="selected">
+                    <div class="option">
+                        <i class="fa-solid fa-landmark" title="Inicio"></i>
+                        <h4>Inicio</h4>
+                    </div>
+                </a>
+
+                <a href=" /resources/views/admin/admin_saldo/saldo_admin.php">
+                    <div class="option">
+                        <i class="fa-solid fa-sack-dollar" title=""></i>
+                        <h4>Saldo Incial</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/usuarios/crudusuarios.php">
+                    <div class="option">
+                        <i class="fa-solid fa-users" title=""></i>
+                        <h4>Usuarios</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/usuarios/registrar.php">
+                    <div class="option">
+                        <i class="fa-solid fa-user-plus" title=""></i>
+                        <h4>Registrar Usuario</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/clientes/lista_clientes.php">
+                    <div class="option">
+                        <i class="fa-solid fa-people-group" title=""></i>
+                        <h4>Clientes</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/clientes/agregar_clientes.php">
+                    <div class="option">
+                        <i class="fa-solid fa-user-tag" title=""></i>
+                        <h4>Registrar Clientes</h4>
+                    </div>
+                </a>
+                <a href="/resources/views/admin/creditos/crudPrestamos.php">
+                    <div class="option">
+                        <i class="fa-solid fa-hand-holding-dollar" title=""></i>
+                        <h4>Prestamos</h4>
+                    </div>
+                </a>
+                <a href="/resources/views/admin/creditos/prestamos.php">
+                    <div class="option">
+                        <i class="fa-solid fa-file-invoice-dollar" title=""></i>
+                        <h4>Registrar Prestamos</h4>
+                    </div>
+                </a>
+                <a href="/resources/views/admin/cobros/cobros.php">
+                    <div class="option">
+                        <i class="fa-solid fa-arrow-right-to-city" title=""></i>
+                        <h4>Zonas de cobro</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/gastos/gastos.php">
+                    <div class="option">
+                        <i class="fa-solid fa-sack-xmark" title=""></i>
+                        <h4>Gastos</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/abonos/lista_super.php">
+                    <div class="option">
+                        <i class="fa-solid fa-map" title=""></i>
+                        <h4>Ruta</h4>
+                    </div>
+                </a>
+
+                <a href="/resources/views/admin/abonos/abonos.php">
+                    <div class="option">
+                        <i class="fa-solid fa-money-bill-trend-up" title=""></i>
+                        <h4>Abonos</h4>
+                    </div>
+                </a>
+                <a href="/resources/views/admin/retiros/retiros.php">
+                    <div class="option">
+                        <i class="fa-solid fa-scale-balanced" title=""></i>
+                        <h4>Retiros</h4>
+                    </div>
+                </a>
             </div>
-        </a>
-
-        <a href="/resources/views/admin/usuarios/registrar.php">
-            <div class="option">
-                <i class="fa-solid fa-user-plus" title=""></i>
-                <h4>Registrar Usuario</h4>
-            </div>
-        </a>
-
-        <a href="/resources/views/admin/clientes/lista_clientes.php">
-            <div class="option">
-                <i class="fa-solid fa-people-group" title=""></i>
-                <h4>Clientes</h4>
-            </div>
-        </a>
-
-        <a href="/resources/views/admin/clientes/agregar_clientes.php">
-            <div class="option">
-                <i class="fa-solid fa-user-tag" title=""></i>
-                <h4>Registrar Clientes</h4>
-            </div>
-        </a>
-        <a href="/resources/views/admin/creditos/crudPrestamos.php">
-            <div class="option">
-                <i class="fa-solid fa-hand-holding-dollar" title=""></i>
-                <h4>Prestamos</h4>
-            </div>
-        </a>
-        <a href="/resources/views/admin/creditos/prestamos.php">
-            <div class="option">
-                <i class="fa-solid fa-file-invoice-dollar" title=""></i>
-                <h4>Registrar Prestamos</h4>
-            </div>
-        </a>
-        <a href="/resources/views/admin/cobros/cobros.php">
-            <div class="option">
-                <i class="fa-solid fa-arrow-right-to-city" title=""></i>
-                <h4>Zonas de cobro</h4>
-            </div>
-        </a>
-
-        <a href="/resources/views/admin/gastos/gastos.php">
-            <div class="option">
-                <i class="fa-solid fa-sack-xmark" title=""></i>
-                <h4>Gastos</h4>
-            </div>
-        </a>
-
-        <a href="/resources/views/admin/abonos/lista_super.php">
-            <div class="option">
-                <i class="fa-solid fa-map" title=""></i>
-                <h4>Ruta</h4>
-            </div>
-        </a>
-
-        <a href="/resources/views/admin/abonos/abonos.php">
-            <div class="option">
-                <i class="fa-solid fa-money-bill-trend-up" title=""></i>
-                <h4>Abonos</h4>
-            </div>
-        </a>
-        <a href="/resources/views/admin/retiros/retiros.php">
-            <div class="option">
-                <i class="fa-solid fa-scale-balanced" title=""></i>
-                <h4>Retiros</h4>
-            </div>
-        </a>
-    </div>
-</div>
+        </div>
 
 
-    <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
+        <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
 
-    <main>  
-        <!-- Botón para volver a la página anterior -->
-    <h1 class="text-center">Listado de Supervisores</h1>
+        <main>
+            <!-- Botón para volver a la página anterior -->
+            <h1 class="text-center">Listado de Supervisores</h1>
 
-<div class="container-fluid"> 
+            <div class="container-fluid">
 
-        <!-- Resto del código de la tabla --> 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Apellido</th> 
-                    <th scope="col">Zona</th>
-                    <th scope="col">Rol</th> 
-                    <th scope="col">Acciones</th> 
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+                <!-- Resto del código de la tabla -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Zona</th>
+                            <th scope="col">Rol</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                 include("../../../../controllers/conexion.php");
                 $sql = $conexion->prepare("SELECT Usuarios.ID, Usuarios.Nombre, Usuarios.Apellido, Usuarios.Email, Zonas.Nombre AS Zona, Roles.Nombre AS Rol FROM Usuarios JOIN Zonas ON Usuarios.Zona = Zonas.ID JOIN Roles ON Usuarios.RolID = Roles.ID WHERE Roles.ID = 2"); // Filtra por el ID del rol de supervisor (2)
                 
@@ -178,35 +179,40 @@ if (isset($_GET['mensaje'])) {
                 while ($datos = $result->fetch_object()) { 
                     $rowCount++; // Incrementar el contador de filas
                     ?>
-                    <tr class="row<?= $rowCount ?>">
-                        <td><?= "REC 100" .$datos->ID ?></td>
-                        <td><?= $datos->Nombre ?></td>
-                        <td><?= $datos->Apellido ?></td>
-                        <td><?= $datos->Zona ?></td>
-                        <td><?= $datos->Rol ?></td> 
-                        <td>
-                            <!-- Botón para ver los cobradores de la zona -->
-                            <a href="ver_cobradores.php?zona=<?= urlencode($datos->Zona) ?>" class="btn btn-primary">Ver Cobradores</a>
-                            <a href="ver_prestamos.php?zona=<?= urlencode($datos->Zona) ?>" class="btn btn-primary">Ver Prestamos</a>
-                            <a href="ruta.php?zona=<?= urlencode($datos->Zona) ?>" class="btn btn-primary">Ruta</a>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+                        <tr class="row<?= $rowCount ?>">
+                            <td><?= "REC 100" .$datos->ID ?></td>
+                            <td><?= $datos->Nombre ?></td>
+                            <td><?= $datos->Apellido ?></td>
+                            <td><?= $datos->Zona ?></td>
+                            <td><?= $datos->Rol ?></td>
+                            <td>
+                                <!-- Botón para ver los cobradores de la zona -->
+                                <a href="ver_cobradores.php?zona=<?= urlencode($datos->Zona) ?>"
+                                    class="btn btn-primary">Ver Cobradores</a>
+                                <a href="ver_prestamos.php?zona=<?= urlencode($datos->Zona) ?>"
+                                    class="btn btn-primary">Ver Prestamos</a>
+                                <a href="ruta.php?zona=<?= urlencode($datos->Zona) ?>" class="btn btn-primary">Ruta</a>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
 
-    <!-- Paginación -->
-    <div id="pagination" class="text-center">
-        <ul class="pagination">
-            <!-- Los botones de paginación se generarán aquí -->
-        </ul>
-    </div>
-</div>
-    </main>
+            <!-- Paginación -->
+            <div id="pagination" class="text-center">
+                <ul class="pagination">
+                    <!-- Los botones de paginación se generarán aquí -->
+                </ul>
+            </div>
+            </div>
+        </main>
 
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src="/menu/main.js"></script>
+        <script src="/public/assets/js/MenuLate.js"></script>
 
- 
+
+
+
+    </body>
+
+</html>
