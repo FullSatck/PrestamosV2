eb1ec4a7fa54ab08e8afaede986e4930848a3c4a
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-11-07 14:38:03 -0500

    hhhhh

--- resources/views/supervisor/usuarios/registrar.php
+++ resources/views/supervisor/usuarios/registrar.php
@@ -1,6 +1,4 @@
 <?php
-
- 
 session_start();
 include("../../../../controllers/conexion.php");
 
@@ -12,126 +10,132 @@ if (isset($_SESSION["usuario_id"])) {
     header("Location: ../../../../index.php");
     exit();
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar Usuario</title>
+
     <link rel="stylesheet" href="/public/assets/css/registrar_usuarios.css">
-    <title>Registro de Usuario</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
-<body>
-    <div class="menu">
-        <ion-icon name="menu-outline"></ion-icon>
-        <ion-icon name="close-circle-outline"></ion-icon>
-    </div>
-    <div class="barra-lateral">
-        <div>
-            <div class="nombre-pagina">
-                <ion-icon id="cloud" name="wallet-outline"></ion-icon>
-                <span>Recaudo</span>
-            </div>
+<body id="body">
+
+<header>
+        <div class="icon__menu">
+            <i class="fas fa-bars" id="btn_open"></i>
         </div>
-        <nav class="navegacion">
-            <ul> 
-                <li>
-                    <a href="/resources/views/admin/inicio/inicio.php">
-                        <ion-icon name="home-outline"></ion-icon>
-                        <span>Inicio</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/usuarios/crudusuarios.php">
-                        <ion-icon name="people-outline"></ion-icon>
-                        <span>Usuarios</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/usuarios/registrar.php" class="hola">
-                        <ion-icon name="person-add-outline"></ion-icon>
-                        <span>Registrar Usuario</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/clientes/lista_clientes.php">
-                        <ion-icon name="people-circle-outline"></ion-icon>
-                        <span>Clientes</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/clientes/agregar_clientes.php">
-                        <ion-icon name="person-circle-outline"></ion-icon>
-                        <span>Registrar Clientes</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/creditos/crudPrestamos.php">
-                        <ion-icon name="list-outline"></ion-icon>
-                        <span>Prestamos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/creditos/prestamos.php">
-                        <ion-icon name="cloud-upload-outline"></ion-icon>
-                        <span>Registrar Prestamos</span>
-                    </a>
-                </li> 
-                <li>
-                    <a href="/resources/views/admin/cobros/cobros.php">
-                        <ion-icon name="planet-outline"></ion-icon>
-                        <span>Zonas de cobro</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/gastos/gastos.php">
-                        <ion-icon name="alert-circle-outline"></ion-icon>
-                        <span>Gastos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/abonos/lista_super.php">
-                        <ion-icon name="map-outline"></ion-icon>
-                        <span>Ruta</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/abonos/abonos.php">
-                        <ion-icon name="cloud-download-outline"></ion-icon>
-                        <span>Abonos</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/resources/views/admin/retiros/retiros.php">
-                        <ion-icon name="cloud-done-outline"></ion-icon>
-                        <span>Retiros</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+    </header> 
 
-        <div>
-            <div class="linea"></div>
+    <div class="menu__side" id="menu_side">
 
-            <div class="modo-oscuro">
-                <div class="info">
-                    <ion-icon name="arrow-back-outline"></ion-icon>
-                    <a href="/controllers/cerrar_sesion.php"><span>Cerrar Sesion</span></a>
+        <div class="name__page">
+            <img src="/public/assets/img/logo.png" class="img logo-image" alt="">
+            <h4>Recaudo</h4>
+        </div>
+
+        <div class="options__menu">
+
+            <a href="/resources/views/admin/inicio/inicio.php">
+                <div class="option">
+                    <i class="fa-solid fa-landmark" title="Inicio"></i>
+                    <h4>Inicio</h4>
                 </div>
-            </div>
+            </a>
+
+            <a href=" /resources/views/admin/admin_saldo/saldo_admin.php">
+                <div class="option">
+                    <i class="fa-solid fa-sack-dollar" title=""></i>
+                    <h4>Saldo Incial</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/usuarios/crudusuarios.php">
+                <div class="option">
+                    <i class="fa-solid fa-users" title=""></i>
+                    <h4>Usuarios</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/usuarios/registrar.php" class="selected">
+                <div class="option">
+                    <i class="fa-solid fa-user-plus" title=""></i>
+                    <h4>Registrar Usuario</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/clientes/lista_clientes.php">
+                <div class="option">
+                    <i class="fa-solid fa-people-group" title=""></i>
+                    <h4>Clientes</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/clientes/agregar_clientes.php">
+                <div class="option">
+                    <i class="fa-solid fa-user-tag" title=""></i>
+                    <h4>Registrar Clientes</h4>
+                </div>
+            </a>
+            <a href="/resources/views/admin/creditos/crudPrestamos.php">
+                <div class="option">
+                    <i class="fa-solid fa-hand-holding-dollar" title=""></i>
+                    <h4>Prestamos</h4>
+                </div>
+            </a>
+            <a href="/resources/views/admin/creditos/prestamos.php">
+                <div class="option">
+                    <i class="fa-solid fa-file-invoice-dollar" title=""></i>
+                    <h4>Registrar Prestamos</h4>
+                </div>
+            </a>
+            <a href="/resources/views/admin/cobros/cobros.php">
+                <div class="option">
+                    <i class="fa-solid fa-arrow-right-to-city" title=""></i>
+                    <h4>Zonas de cobro</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/gastos/gastos.php">
+                <div class="option">
+                    <i class="fa-solid fa-sack-xmark" title=""></i>
+                    <h4>Gastos</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/ruta/lista_super.php">
+                <div class="option">
+                    <i class="fa-solid fa-map" title=""></i>
+                    <h4>Ruta</h4>
+                </div>
+            </a>
+
+            <a href="/resources/views/admin/abonos/abonos.php">
+                <div class="option">
+                    <i class="fa-solid fa-money-bill-trend-up" title=""></i>
+                    <h4>Abonos</h4>
+                </div>
+            </a>
+            <a href="/resources/views/admin/retiros/retiros.php">
+                <div class="option">
+                    <i class="fa-solid fa-scale-balanced" title=""></i>
+                    <h4>Retiros</h4>
+                </div>
+            </a>
+
         </div>
 
     </div>
-
+    <script src="/public/assets/js/MenuLate.js"></script>
 
     <!-- ACA VA EL CONTENIDO DE LA PAGINA -->
-
     <main>
         <!-- Contenido principal -->
         <main>
@@ -143,8 +147,7 @@ if (isset($_SESSION["usuario_id"])) {
                 </div>
                 <div class="input-container">
                     <label for "apellido">Apellido:</label>
-                    <input type="text" id="apellido" name="apellido" placeholder="Por favor ingrese su apellido"
-                        required>
+                    <input type="text" id="apellido" name="apellido" placeholder="Por favor ingrese su apellido" required>
                 </div>
                 <div class="input-container">
                     <label for="email">Correo Electrónico:</label>
@@ -152,23 +155,22 @@ if (isset($_SESSION["usuario_id"])) {
                 </div>
                 <div class="input-container">
                     <label for="contrasena">Contraseña:</label>
-                    <input type="password" id="contrasena" name="contrasena" placeholder="Por favor ingrese su clave"
-                        required>
+                    <input type="password" id="contrasena" name="contrasena" placeholder="Por favor ingrese su clave" required>
                 </div>
                 <div class="input-container">
                     <label for="zona">Zona:</label>
                     <select id="zona" name="zona" required>
                         <?php
-                    // Incluye el archivo de conexión a la base de datos
-                    include("../../../../controllers/conexion.php");
-                    // Consulta SQL para obtener las zonas
-                    $consultaZonas = "SELECT ID, Nombre FROM zonas";
-                    $resultZonas = mysqli_query($conexion, $consultaZonas);
-                    // Genera las opciones del menú desplegable para Zona
-                    while ($row = mysqli_fetch_assoc($resultZonas)) {
-                        echo '<option value="' . $row['ID'] . '">' . $row['Nombre'] . '</option>';
-                    }
-                    ?>
+                        // Incluye el archivo de conexión a la base de datos
+                        include("../../../../controllers/conexion.php");
+                        // Consulta SQL para obtener las zonas
+                        $consultaZonas = "SELECT ID, Nombre FROM zonas";
+                        $resultZonas = mysqli_query($conexion, $consultaZonas);
+                        // Genera las opciones del menú desplegable para Zona
+                        while ($row = mysqli_fetch_assoc($resultZonas)) {
+                            echo '<option value="' . $row['ID'] . '">' . $row['Nombre'] . '</option>';
+                        }
+                        ?>
                     </select>
                 </div>
 
@@ -176,21 +178,20 @@ if (isset($_SESSION["usuario_id"])) {
                     <label for="RolID">Rol:</label>
                     <select id="RolID" name="RolID" required>
                         <?php
-                    // Consulta SQL para obtener las opciones de roles
-                    $consultaRoles = "SELECT ID, Nombre FROM Roles";
-                    $resultRoles = mysqli_query($conexion, $consultaRoles);
-                    // Genera las opciones del menú desplegable para Rol
-                    while ($row = mysqli_fetch_assoc($resultRoles)) {
-                        echo '<option value="' . $row['ID'] . '">' . $row['Nombre'] . '</option>';
-                    }
-                    ?>
+                        // Consulta SQL para obtener las opciones de roles
+                        $consultaRoles = "SELECT ID, Nombre FROM Roles WHERE ID = 3";
+                        $resultRoles = mysqli_query($conexion, $consultaRoles);
+                        // Genera las opciones del menú desplegable para Rol
+                        while ($row = mysqli_fetch_assoc($resultRoles)) {
+                            echo '<option value="' . $row['ID'] . '">' . $row['Nombre'] . '</option>';
+                        }
+                        ?>
                     </select>
                 </div>
 
                 <div class="input-container" id="saldo-inicial-container" style="display: none;">
                     <label for="saldo-inicial">Saldo Inicial:</label>
-                    <input type="text" id="saldo-inicial" name="saldo-inicial"
-                        placeholder="Por favor ingrese el saldo inicial">
+                    <input type="text" id="saldo-inicial" name="saldo-inicial" placeholder="Por favor ingrese el saldo inicial">
                 </div>
 
                 <div class="btn-container">
@@ -202,7 +203,6 @@ if (isset($_SESSION["usuario_id"])) {
     </main>
 
     <script>
-    // Agrega un evento para detectar cambios en la selección del rol
     $(document).ready(function() {
         $("#RolID").on("change", function() {
             var selectedRole = $(this).val();
@@ -215,9 +215,6 @@ if (isset($_SESSION["usuario_id"])) {
         });
     });
     </script>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src="/menu/main.js"></script>
 
 </body>
 
