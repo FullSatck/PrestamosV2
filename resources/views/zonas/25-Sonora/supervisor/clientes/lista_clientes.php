4895a3272a95f1038728f74f83fffa6f939e54ce
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-11-09 13:26:36 -0500

    aaaa

--- resources/views/zonas/25-Sonora/supervisor/clientes/lista_clientes.php
+++ resources/views/zonas/25-Sonora/supervisor/clientes/lista_clientes.php
@@ -22,7 +22,7 @@ include("../../../../../../controllers/conexion.php");
 $sql = "SELECT c.ID, c.Nombre, c.Apellido, c.Domicilio, c.Telefono, c.HistorialCrediticio, c.ReferenciasPersonales, m.Nombre AS Moneda, c.ZonaAsignada 
         FROM Clientes c
         LEFT JOIN Monedas m ON c.MonedaPreferida = m.ID
-        WHERE c.ZonaAsignada = 'Aguascalientes'";
+        WHERE c.ZonaAsignada = 'Sonora'";
 
 $resultado = $conexion->query($sql);
 ?>
@@ -176,7 +176,7 @@ $resultado = $conexion->query($sql);
                 <td><?= $fila["ZonaAsignada"] ?></td>
                 <td><a href="../../../../../../controllers/perfil_cliente.php?id=<?= $fila["ID"] ?>">Perfil</a></td>
                 <td><a
-                        href="/resources/views/zonas/1-aguascalientes/supervisor/abonos/crud_historial_pagos.php?clienteId=<?= $fila["ID"] ?>">pagos</a>
+                        href="/resources/views/zonas/25-Sonora/supervisor/abonos/crud_historial_pagos.php?clienteId=<?= $fila["ID"] ?>">pagos</a>
                 </td>
             </tr>
             <?php } ?>
--- resources/views/zonas/25-Sonora/supervisor/creditos/prestamos.php
+++ resources/views/zonas/25-Sonora/supervisor/creditos/prestamos.php
@@ -132,7 +132,7 @@ if (isset($_SESSION["usuario_id"])) {
 
     <main>
         <h1>Solicitud de Préstamo</h1><br><br>
-        <form action="/controllers/super/procesar_prestamo.php" method="POST" class="form-container">
+        <form action="/controllers/super/procesar_prestamo/procesar_prestamo1.php" method="POST" class="form-container">
             <?php
             // Incluir el archivo de conexión a la base de datos
             include("../../../../../../controllers/conexion.php");
