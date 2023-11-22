1dcdfa637bcbc5518953ed7fe0db7012c15621a4
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-11-21 23:22:20 -0500

    gastos de puebla

--- resources/views/zonas/20-Puebla/cobrador/gastos/gastos.php
+++ resources/views/zonas/20-Puebla/cobrador/gastos/gastos.php
@@ -8,7 +8,7 @@ if (isset($_SESSION["usuario_id"])) {
     // El usuario no está autenticado, redirige a la página de inicio de sesión
     header("Location: ../../../../../../index.php");
     exit();
-}
+} 
 
 
 // El usuario ha iniciado sesión, mostrar el contenido de la página aquí
@@ -18,12 +18,12 @@ if (isset($_SESSION["usuario_id"])) {
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <script src="https://kit.fontawesome.com/9454e88444.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/public/assets/css/gastos.css">
-    <title>Lista de Gastos</title> 
+    <title>Lista de Gastos</title>
 </head>
 
 <body id="body">
@@ -54,7 +54,7 @@ if (isset($_SESSION["usuario_id"])) {
                 </div>
             </a>
 
-      
+
             <a href="/resources/views/zonas/20-Puebla/cobrador/clientes/lista_clientes.php">
                 <div class="option">
                     <i class="fa-solid fa-people-group" title=""></i>
@@ -110,7 +110,7 @@ if (isset($_SESSION["usuario_id"])) {
                     <h4>Abonos</h4>
                 </div>
             </a>
- 
+
 
 
 
@@ -124,55 +124,59 @@ if (isset($_SESSION["usuario_id"])) {
     <main>
         <h1>Lista de Gastos</h1>
         <div class="table-scroll-container">
-        <?php
-// Incluye la configuración de conexión a la base de datos
-include "../../../../../../controllers/conexion.php"; // Asegúrate de que la ruta sea correcta
+            <?php
+        // Incluye la configuración de conexión a la base de datos
+        include "../../../../../../controllers/conexion.php"; // Asegúrate de que la ruta sea correcta
 
-// Realiza la consulta para obtener los gastos con el nombre de la zona
-$sql = "SELECT g.ID, z.Nombre AS nombreZona, g.Fecha, g.Descripcion, g.Valor 
-        FROM gastos g
-        INNER JOIN zonas z ON g.IDZona = z.ID
-        WHERE iDZona = 20
-        ORDER BY g.ID DESC";
-$resultado = $conexion->query($sql);
+        // Realiza la consulta para obtener los gastos con el nombre de la zona
+        $sql = "SELECT g.ID, z.Nombre AS nombreZona, g.Ciudad, g.Asentamiento, g.Fecha, g.Descripcion, g.Valor
+                FROM gastos g
+                INNER JOIN zonas z ON g.IDZona = z.ID
+                WHERE g.IDZona = 20
+                ORDER BY g.ID DESC";
+        $resultado = $conexion->query($sql);
 
-
-// Crear una tabla HTML para mostrar las columnas de las filas
-echo '<table>';
-echo '<tr>';
-echo '<th>ID</th>';
-echo '<th>Zona</th>';
-echo '<th>Fecha</th>';
-echo '<th>Descripción</th>';
-echo '<th>Valor</th>';
-echo '</tr>';
-
-// Verifica si hay gastos en la base de datos
-if ($resultado->num_rows > 0) {
-    // Si hay gastos, itera a través de los resultados y muestra cada gasto
-    while ($fila = $resultado->fetch_assoc()) {
+        // Crear una tabla HTML para mostrar las columnas de las filas
+        echo '<table>';
         echo '<tr>';
-        echo '<td>' . $fila['ID'] . '</td>';
-        echo '<td>' . $fila['nombreZona'] . '</td>';
-        echo '<td>' . $fila['Fecha'] . '</td>';
-        echo '<td>' . $fila['Descripcion'] . '</td>'; 
-        echo "<td>" . number_format($fila['Valor'], 0, '.', '.') . "</td>"; // Formatear el monto
+        echo '<th>ID</th>';
+        echo '<th>Estado</th>';
+        echo '<th>Municipio</th>';
+        echo '<th>Colonia</th>';
+        echo '<th>Fecha</th>';
+        echo '<th>Descripción</th>';
+        echo '<th>Valor</th>';
         echo '</tr>';
-    }
-} else {
-    // Si no hay gastos, muestra una fila con celdas vacías
-    echo '<tr>';
-    echo '<td colspan="5">No se encontraron gastos en la base de datos.</td>';
-    echo '</tr>';
-}
 
-echo '</table>';
+        // Verifica si hay gastos en la base de datos
+        if ($resultado->num_rows > 0) {
+            // Si hay gastos, itera a través de los resultados y muestra cada gasto
+            while ($fila = $resultado->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $fila['ID'] . '</td>';
+                echo '<td>' . $fila['nombreZona'] . '</td>';
+                echo '<td>' . $fila['Ciudad'] . '</td>'; // Asegúrate de que 'Ciudad' corresponda al nombre de la columna
+                echo '<td>' . $fila['Asentamiento'] . '</td>'; // Asegúrate de que 'Asentamiento' corresponda al nombre de la columna
+                echo '<td>' . $fila['Fecha'] . '</td>';
+                echo '<td>' . $fila['Descripcion'] . '</td>'; 
+                echo "<td>" . number_format($fila['Valor'], 0, '.', '.') . "</td>"; // Formatear el monto
+                echo '</tr>';
+            }
+        } else {
+            // Si no hay gastos, muestra una fila con celdas vacías
+            echo '<tr>';
+            echo '<td colspan="7">No se encontraron gastos en la base de datos.</td>';
+            echo '</tr>';
+        }
 
-// Cierra la conexión a la base de datos
-$conexion->close();
-?>
-</div>
+        echo '</table>';
+
+        // Cierra la conexión a la base de datos
+        $conexion->close();
+        ?>
+        </div>
     </main>
+
 
 
 
