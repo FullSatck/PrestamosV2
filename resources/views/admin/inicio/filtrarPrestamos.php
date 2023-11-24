d44590e97c4229e2d87ffd5077053459b724e3cf
SaturnoXVI <jcbohorquez712@gmail.com>
2023-11-24 17:05:29 -0500

    a

--- resources/views/admin/inicio/filtrarPrestamos.php
+++ resources/views/admin/inicio/filtrarPrestamos.php
@@ -1,57 +1,72 @@
 <?php
 require '../../../../controllers/conexion.php';
 
-function obtenerCuotas($conn, $filtro) {
+function obtenerCuotas($conexion, $filtro) {
     $fechaHoy = date('Y-m-d');
-    $diaHoy = date('j'); // Día del mes sin ceros iniciales
-    $cuotas = [];
+    $cuotas = array();
 
-    // Iniciar la consulta SQL base
-    $sql = "SELECT p.ID, p.IDCliente, p.MontoCuota, p.FechaInicio, p.FrecuenciaPago
+    // Consulta para obtener los préstamos y verificar los pagos realizados
+    $sql = "SELECT p.ID, p.IDCliente, p.MontoCuota, p.FechaInicio, p.FrecuenciaPago,
+                   (SELECT COUNT(*) FROM historial_pagos WHERE IDPrestamo = p.ID AND FechaPago = ?) as PagadoHoy,
+                   (SELECT SUM(MontoPagado) FROM historial_pagos WHERE IDPrestamo = p.ID) as TotalPagado
             FROM prestamos p
-            WHERE p.FechaInicio <= ?";
-
-    // Agregar filtro de estado
-    if ($filtro == 'pagado') {
-        $sql .= " AND p.Estado = 'pagado'";
-    } elseif ($filtro == 'pendiente') {
-        $sql .= " AND p.Estado = 'pendiente'";
-    } elseif ($filtro == 'nopagado') {
-        $sql .= " AND p.Estado = 'nopagado'";
+            WHERE p.FechaInicio <= ? AND p.Estado != 'pagado'";
+    // Preparar la consulta SQL
+    $stmt = $conexion->prepare($sql);
+    if (!$stmt) {
+        echo "Error al preparar la consulta: " . $conexion->error;
+        return array();
     }
+    $stmt->bind_param("ss", $fechaHoy, $fechaHoy);
 
-    // Preparar la consulta SQL sin la parte de la frecuencia de pago
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $fechaHoy);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
+    // Ejecutar la consulta
+    if ($stmt->execute()) {
+        $resultado = $stmt->get_result();
 
-    while ($fila = $resultado->fetch_assoc()) {
-        // Determinar si hoy se debe pagar según la frecuencia de pago
-        $diasDiferencia = (new DateTime($fechaHoy))->diff(new DateTime($fila['FechaInicio']))->days;
-        
-        $debePagarHoy = false;
-        switch ($fila['FrecuenciaPago']) {
-            case 'diario':
-                $debePagarHoy = true; // Todos los días
-                break;
-            case 'semanal':
-                $debePagarHoy = ($diasDiferencia % 7) == 0;
-                break;
-            case 'quincenal':
-                $debePagarHoy = ($diasDiferencia % 14) == 0;
-                break;
-            case 'mensual':
-                $diaInicio = (new DateTime($fila['FechaInicio']))->format('j');
-                $debePagarHoy = $diaInicio == $diaHoy;
-                break;
-        }
+        while ($fila = $resultado->fetch_assoc()) {
+            // Verificar si la cuota ha sido pagada hoy
+            if ($filtro == 'pagado' && $fila['PagadoHoy'] == 0) continue;
+            if ($filtro == 'pendiente' && $fila['PagadoHoy'] > 0) continue;
 
-        if ($debePagarHoy) {
+            // Calcular si hoy es un día de pago según la frecuencia
+            if (!esDiaDePago($fila['FechaInicio'], $fila['FrecuenciaPago'], $fechaHoy)) continue;
+
+            // Agregar la fila al array de cuotas
             $cuotas[] = $fila;
         }
+    } else {
+        // Manejar el error en la ejecución de la consulta
+        echo "Error al ejecutar la consulta: " . $stmt->error;
     }
 
+    // Cerrar la declaración preparada
     $stmt->close();
+
     return $cuotas;
 }
+
+function esDiaDePago($fechaInicio, $frecuenciaPago, $fechaHoy) {
+    $fechaInicio = new DateTime($fechaInicio);
+    $fechaHoy = new DateTime($fechaHoy);
+
+    $intervalo = $fechaInicio->diff($fechaHoy);
+
+    switch ($frecuenciaPago) {
+        case 'diario':
+            return true;
+        case 'semanal':
+            return $intervalo->days % 7 == 0;
+        case 'quincenal':
+            return $intervalo->days % 15 == 0;
+        case 'mensual':
+            return $fechaInicio->format('d') == $fechaHoy->format('d');
+        default:
+            return false;
+    }
+}
+
+// Ejemplo de uso
+$cuotasPendientes = obtenerCuotas($conexion, 'pendiente');
+$cuotasPagadas = obtenerCuotas($conexion, 'pagado');
+
+?>
