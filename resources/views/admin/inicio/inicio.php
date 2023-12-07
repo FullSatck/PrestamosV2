1989e677a6d3d4bef8f2ab1e54567131802588a1
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-12-07 15:43:57 -0500

    a

--- resources/views/admin/inicio/inicio.php
+++ resources/views/admin/inicio/inicio.php
@@ -238,17 +238,18 @@ date_default_timezone_set('America/Bogota');
                    
                 </div>
 
-                <div class="cuadro cuadro-2">
+               
+
+
+
+        </div>
+        <div class="cuadro cuadro-2">
                 <div class="cuadro-1-1">
                     <a href="/controllers/super/ListaClavos.php" class="titulo">Lista Clavos </a>
                    
                 </div>
 
             </div>
-
-
-
-        </div>
 
 
         </div>
