8a6ed370f9d68ea03d49317c1d2209e0acea2a16
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-12-17 21:05:30 -0500

    Update inicio.php

--- resources/views/zonas/6-Chihuahua/cobrador/inicio/inicio.php
+++ resources/views/zonas/6-Chihuahua/cobrador/inicio/inicio.php
@@ -166,7 +166,7 @@ date_default_timezone_set('America/Bogota');
 
             <div class="cuadro cuadro-2">
                 <div class="cuadro-1-1">
-                    <a href="/resources/views/zonas/6-Chihuahua/cobrador/inicio/prestadia/prestamos_del_dia.php" class="titulo">Prestamos del dia </a>
+                    <a href="/resources/views/zonas/6-Chihuahua/cobrador/inicio/prestadia/prestamos_del_dia.php" class="titulo">Filtros</a>
                     <p>Version beta</p>
                 </div>
             </div>
