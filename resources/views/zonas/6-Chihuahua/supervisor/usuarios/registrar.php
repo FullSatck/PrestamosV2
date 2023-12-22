7fd0246c2a79b33cdef3d746e796544930ddcad7
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-12-21 19:39:50 -0500

    supervisor -- 6 listo

--- resources/views/zonas/6-Chihuahua/supervisor/usuarios/registrar.php
+++ resources/views/zonas/6-Chihuahua/supervisor/usuarios/registrar.php
@@ -136,7 +136,7 @@ $stmt->close();
         <!-- Contenido principal -->
         <main>
             <h2>Registro de Usuario</h2><br>
-            <form action="/controllers/super/validar_registro/validar_registro-20.php" method="post">
+            <form action="/controllers/super/validar_registro/validar_registro-6.php" method="post">
                 <div class="input-container">
                     <label for="nombre">Nombre:</label>
                     <input type="text" id="nombre" name="nombre" placeholder="Por favor ingrese su nombre" required>
