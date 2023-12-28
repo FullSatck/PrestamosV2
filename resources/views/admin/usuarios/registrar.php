6fae01cf260e1a1b9a0c36294a847bfde7ea0060
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2023-12-28 03:23:36 -0500

    admin

--- resources/views/admin/usuarios/registrar.php
+++ resources/views/admin/usuarios/registrar.php
@@ -158,15 +158,7 @@ date_default_timezone_set('America/Bogota');
                     <i class="fa-solid fa-sack-xmark" title=""></i>
                     <h4>Gastos</h4>
                 </div>
-            </a>
-
-            <a href="/resources/views/admin/ruta/ruta.php">
-                <div class="option">
-                    <i class="fa-solid fa-map" title=""></i>
-                    <h4>Enrutar</h4>
-                </div>
-            </a>
-
+            </a> 
 
             <a href="/resources/views/admin/retiros/retiros.php">
                 <div class="option">
