bc2c754f41bd03b2dd1569391a68681e0e58657b
SaturnoXVI <130761712+SaturnoXVI@users.noreply.github.com>
2023-12-31 10:46:01 -0500

    1

--- resources/views/admin/inicio/prestadia/prestamos_del_dia.php
+++ resources/views/admin/inicio/prestadia/prestamos_del_dia.php
@@ -168,7 +168,7 @@ date_default_timezone_set('America/Bogota');
                                 <td><?php echo htmlspecialchars($cuota['IdentificacionCURP']); ?></td>
                                 <td><?php echo htmlspecialchars($cuota['TelefonoCliente']); ?></td>
                                 <td>
-                                    <a href="../../../../../controllers/perfil_abonos.php?id=<?php echo $cuota['IDCliente']; ?>" class="btn btn-info">Perfil </a>
+                                    <a href="/resources/views/admin/inicio/cartulina/perfil_abonos.php?id=<?php echo $cuota['IDCliente']; ?>" class="btn btn-info">Perfil </a>
                                     <?php if ($filtro != 'pagado') : ?>
                                 </td>
 
