dd708253fcdf8be434edff59c727142576034d71
St3ven12 <139539450+St3ven12@users.noreply.github.com>
2024-01-04 22:32:44 -0500

    a

--- resources/views/zonas/28-Tlaxcala/supervisor/inicio/cartulina/orden_abonos.php
+++ resources/views/zonas/28-Tlaxcala/supervisor/inicio/cartulina/orden_abonos.php
@@ -106,7 +106,7 @@ $result = $stmt->get_result();
 
         ?>
 
-        <a href="/resources/views/zonas/20-Puebla/supervisor/inicio/cartulina/perfil_abonos.php?id=<?= $primer_id ?>" class="back-link1">Volver</a>
+        <a href="/resources/views/zonas/28-Tlaxcala/supervisor/inicio/cartulina/perfil_abonos.php?id=<?= $primer_id ?>" class="back-link1">Volver</a>
 
         <div class="nombre-usuario">
             <?php
