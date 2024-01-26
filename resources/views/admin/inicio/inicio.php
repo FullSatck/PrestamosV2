9b6ea9c5937eb563493325d79df60654a678d386
duartrufas <130761791+duartrufas@users.noreply.github.com>
2024-01-26 15:44:53 -0500

    .

--- resources/views/admin/inicio/inicio.php
+++ resources/views/admin/inicio/inicio.php
@@ -238,18 +238,17 @@ date_default_timezone_set('America/Bogota');
                    
                 </div>
 
-               
-
-
-
-        </div>
-        <div class="cuadro cuadro-2">
+                <div class="cuadro cuadro-2">
                 <div class="cuadro-1-1">
-                    <a href="/controllers/super/ListaClavos.php" class="titulo">Lista Clavos </a>
+                    <a href="/controllers/ListaClavos.php" class="titulo">Lista Clavos</a>
                    
                 </div>
 
             </div>
+
+
+
+        </div>
 
 
         </div>
